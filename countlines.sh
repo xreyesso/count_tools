87739484d5770b2414c6f083626b229f06a95171
Xareni Reyes Soto <reyessoto.xareni@gmail.com>
2024-10-31 10:53:22 +0100

    Script improved, it now receives any number of arguments

--- countlines.sh
+++ countlines.sh
@@ -1,10 +1,19 @@
-NLINES=$(grep -c "" $1) #count number of lines in file that the user will input
-if [[ $NLINES -eq 1 ]]
-then 
-	echo The file has 1 line
-elif [[ $NLINES -eq 0 ]]
+if [[ $# -eq 0 ]] # $# gives the count of arguments passed to the script
 then
-	echo The file has 0 lines
-else
-	echo The file has $NLINES lines
+	echo No files were passed as argument
+	exit 1 #exit with error code 1
 fi
+
+for file in $@ # $@ expands to all command-line arguments
+do
+	NLINES=$(grep -c "" $file)
+	if [[ $NLINES -eq 1 ]]
+	then 
+		echo The file has 1 line
+	elif [[ $NLINES -eq 0 ]]
+	then
+		echo The file has 0 lines
+	else
+		echo The file has $NLINES lines
+	fi
+done
